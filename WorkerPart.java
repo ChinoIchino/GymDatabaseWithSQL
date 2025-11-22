50e2a5e368805478eb1c6aa994d1ff612d7232e0
Kamil B. <97180792+ChinoIchino@users.noreply.github.com>
2025-11-22 15:32:25 +0100

    Connection between all the files
    Modified Inscription and WorkerPart to handle some errors, changed a little bit the interface and connected all files with App.
    App can now be used as the main that calls to other parts of the program.

--- WorkerPart.java
+++ WorkerPart.java
@@ -3,34 +3,35 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class WorkerPart {
-    public static void main(String []args) throws Exception{
-        Scanner scanner = new Scanner(System.in);
-        Class.forName("org.postgresql.Driver");
+    public static void main(String []args, Connection con, Scanner scanner) throws Exception{
 
-        Connection con = DriverManager.getConnection("jdbc:postgresql://kafka.iem/kb483753", "kb483753", "kb483753");
-
-        InitDatabase.initAll(con);
-
-        while(true){
-            maintenance(con, scanner);
-        }
-
-        scanner.close();
+        int choice;
+        do{
+            System.out.println("\n//////////////////////////////////////////////\n1. Mettre a jour la prochaine maintenance\n2. Quitter\n");
+            choice = scanner.nextInt();
+            if(choice == 1){
+                maintenance(con, scanner);
+            }
+        }while(choice != 2);
     }
 
     private static void maintenance(Connection con, Scanner scanner) throws Exception{
         
-        System.out.println("Donner l'id de la machine qui viens d'avoir une maintenance : ");
+        System.out.println("\nDonner l'id de la machine qui viens d'avoir une maintenance :\n");
         int idMachine = scanner.nextInt();
-        
-        // scanner.close();
     
         Statement generalStat = con.createStatement();
 
         ResultSet resOfMachine = generalStat.executeQuery("SELECT intervalMaintenanceEnJour FROM proj.machine WHERE id = " + idMachine);
-        resOfMachine.next();
+        boolean isFound = resOfMachine.next();
+
+        if(isFound == false){
+            throw new IllegalArgumentException(
+                "\nWorkerPart.java ERREUR: La machine avec l'id : " + idMachine + " n'a pas etait trouver. Veuillez verifier si l'id de la machine est correcte."
+            );
+        }
+
         int intervalDeMaintenance = resOfMachine.getInt(1);
-        // System.out.println("oldDate = " + oldDate + " // inter = " + intervalDeMaintenance);
         resOfMachine.close();
 
         LocalDate localDate = LocalDate.now().plusDays(intervalDeMaintenance); //ajoute la valeur en jour voulu dans la date actuel
@@ -41,6 +42,7 @@ public class WorkerPart {
 
         //ajoute la maintenance dans la table Maintenance
         generalStat.execute("INSERT INTO proj.Maintenance(id, idMachine, dateDeMaintenance) VALUES ((SELECT COUNT(*) + 1 FROM proj.maintenance), " + idMachine + ", CURRENT_DATE);");
+        System.out.println("\nLa maintenance a bien etait enregistrer.");
 
         generalStat.close();
     }
