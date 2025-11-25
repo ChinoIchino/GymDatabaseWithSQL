b41f349e04835aee116d6753ee08bda9c9c415b1
Kamil B. <97180792+ChinoIchino@users.noreply.github.com>
2025-11-25 16:50:19 +0100

    Final Code Update Commit Of Ver4
    Changed the name of certain files:
        Inscription -> partieClient
        WorkerPart -> partiePro
    fixed some bugs in partieClient and added the function "changerCoach"

--- App.java
+++ App.java
@@ -8,7 +8,7 @@ class App{
         Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection("jdbc:postgresql://kafka.iem/kb483753", "kb483753", "kb483753");
 
-        System.out.print("Voulez-vous reinitialiser la base de données ? (1 = Oui / 0 = Non) : ");
+        System.out.print("Voulez vous reinitialiser la base de donnees ? (1 = Oui / 0 = Non) : ");
         int initChoice = scanner.nextInt();
 
         if(initChoice == 1){
@@ -17,17 +17,16 @@ class App{
 
         int choice = 0;
         while(choice != 3){
-            System.out.println("\n//////////////////////////////////////////////\nQuel menu ouvrir: \n1. Menu Client\n2. Menu Professionnel\n3. Quitter\n");
+            System.out.println("\n//////////////////////////////////////////////\nQuel menu a ouvrir: \n1. Menu Client\n2. Menu Professionnelle\n3. Quitter\n");
             choice = scanner.nextInt();
             
             if(choice == 1){
-                Inscription.main(args, con, scanner);
+                partieClient.main(args, con, scanner);
             }else if(choice == 2){
-                WorkerPart.main(args, con, scanner);
+                partiePro.main(args, con, scanner);
             }
         }
 
         scanner.close();
     }
-
 }
