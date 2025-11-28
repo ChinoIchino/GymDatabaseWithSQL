576f299bb590627de50d76f6e8e7e1809efa7f24
Kamil B. <97180792+ChinoIchino@users.noreply.github.com>
2025-11-28 14:18:37 +0100

    Additional verification
    Added a verification if the project is setup correctly on the user side, if it isn't, it send a SQLException with the description of the error

--- App.java
+++ App.java
@@ -1,3 +1,4 @@
+import java.nio.file.AccessDeniedException;
 import java.sql.*;
 import java.util.Scanner;
 
@@ -8,6 +9,28 @@ class App{
         Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection("jdbc:postgresql://kafka.iem/kb483753", "kb483753", "kb483753");
 
+        // Collecte le nom de tout les schemas de la connection dans un ResultSet
+        DatabaseMetaData metaDataOfDatabase = con.getMetaData();
+        ResultSet stat = metaDataOfDatabase.getSchemas();
+
+        boolean correctSchemaExist = false;
+        // Verifie si le schema du nom de proj existe
+        while(stat.next()){
+            if(stat.getString(1).equals("proj")){
+                correctSchemaExist = true;
+                break;
+            }
+        }
+        
+        // Si il n'a pas été trouver, envoie une erreur associée
+        if(!correctSchemaExist){
+            // Je n'ai pas trouver une erreur adequate, donc j'ai mis SQLException.
+            // Il etait possible de cree sa propre erreur avec un objet qui extend SQLException, cependant je trouvais que ca aurait etait trop hors-sujet par rapport au projet
+            throw new SQLException(
+                "App.java Erreur: Le schema 'proj' n'a pas été trouver, verifier si vous avez sur DBeaver dans le dossier Schemas un schema du nom de 'proj'"
+            );
+        }
+        
         System.out.print("Voulez vous reinitialiser la base de donnees ? (1 = Oui / 0 = Non) : ");
         int initChoice = scanner.nextInt();
 
